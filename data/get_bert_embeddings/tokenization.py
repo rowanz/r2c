e9adede6e07accfc0dcdff7cb9dd25cd6bb3d2bf
cclauss <cclauss@me.com>
2019-01-09 20:13:26 +0100

    Simplify convert_to_unicode() in tokenization.py

--- data/get_bert_embeddings/tokenization.py
+++ data/get_bert_embeddings/tokenization.py
@@ -24,24 +24,14 @@ import six
 import tensorflow as tf
 
 
-def convert_to_unicode(text):
+def convert_to_unicode(text, errors="ignore"):
   """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
-  if six.PY3:
-    if isinstance(text, str):
-      return text
-    elif isinstance(text, bytes):
-      return text.decode("utf-8", "ignore")
-    else:
-      raise ValueError("Unsupported string type: %s" % (type(text)))
-  elif six.PY2:
-    if isinstance(text, str):
-      return text.decode("utf-8", "ignore")
-    elif isinstance(text, unicode):
-      return text
-    else:
-      raise ValueError("Unsupported string type: %s" % (type(text)))
+  if isinstance(text, six.text_type):
+    return text
+  elif isinstance(text, six.binary_type):
+    return text.decode("utf-8", errors)
   else:
-    raise ValueError("Not running on Python2 or Python 3?")
+    raise ValueError("Unsupported string type: %s" % (type(text)))
 
 
 def printable_text(text):
